c3280bf00a0fb946152b77ef799da18e1fb169a0
Valera8 <evaln@mail.ru>
2017-01-20 19:31:17 +0300

    Добавил файлы для заказа, добавил изображения в img, изменил styles.css, index.html

---
+++ lib/orders.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title>Заказы</title>
+</head>
+<body>
+    <h1>Заказы</h1>
+
+    <form action="orders_class.php">
+        <p>Имя:
+        <input type="text" name="name">
+        </p>
+        <p>Email:
+        <input type="email" name="e-mail">
+        </p>
+        <p>Вид услуги:
+        <input type="text" name="service">
+        </p>
+        <p>
+        <input type="submit" name="buy" value="Заказать">
+        </p>
+    </form>
+</body>
+</html>
